295e6da3cfc141c1e335538645ef18bc0c57195d
Daniel Madden <danielfpjobs@gmail.com>
2021-07-20 16:08:03 -0600

    Day 1 Part 2 Complete

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -2,6 +2,14 @@ import { createReducer, on, Action, createSelector } from "@ngrx/store";
 import { BookModel, calculateBooksGrossEarnings } from "src/app/shared/models";
 import { BooksPageActions, BooksApiActions } from "src/app/books/actions";
 
+const createBook = (books: BookModel[], book: BookModel) => [...books, book];
+const updateBook = (books: BookModel[], changes: BookModel) =>
+  books.map((book) => {
+    return book.id === changes.id ? Object.assign({}, book, changes) : book;
+  });
+const deleteBook = (books: BookModel[], bookId: string) =>
+  books.filter((book) => bookId !== book.id);
+
 export interface State {
   collection: BookModel[];
   activeBookId: string | null;
@@ -29,5 +37,47 @@ export const booksReducer = createReducer(
       ...state,
       activeBookid: action.bookId,
     };
+  }),
+  on(BooksApiActions.booksLoaded, (state, action) => {
+    return {
+      ...state,
+      collection: action.books,
+    };
+  }),
+  on(BooksApiActions.bookCreated, (state, action) => {
+    return {
+      ...state,
+      collection: createBook(state.collection, action.book),
+    };
+  }),
+  on(BooksApiActions.bookUpdated, (state, action) => {
+    return {
+      ...state,
+      collection: updateBook(state.collection, action.book),
+    };
+  }),
+  on(BooksApiActions.bookDeleted, (state, action) => {
+    return {
+      ...state,
+      collection: deleteBook(state.collection, action.bookId),
+    };
   })
+);
+
+export function reducer(state: undefined | State, action: Action) {
+  return booksReducer(state, action);
+}
+
+export const selectAll = (state: State) => state.collection;
+export const selectActiveBookId = (state: State) => state.activeBookId;
+
+export const selectActiveBook = createSelector(
+  selectAll,
+  selectActiveBookId,
+  (books, activeBookId) => books.find((book) => book.id === activeBookId)
+);
+
+export const selectEarningsTotals = createSelector(
+  selectAll,
+  calculateBooksGrossEarnings
 );
