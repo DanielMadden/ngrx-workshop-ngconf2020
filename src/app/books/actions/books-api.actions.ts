46e42a6adeffec207a2598a6ec391ddcd91bc4f1
Daniel Madden <danielfpjobs@gmail.com>
2021-07-20 10:57:33 -0600

    api actions

--- src/app/books/actions/books-api.actions.ts
+++ src/app/books/actions/books-api.actions.ts
@@ -1,2 +1,22 @@
 import { createAction, props } from "@ngrx/store";
 import { BookModel } from "src/app/shared/models";
+
+export const booksLoaded = createAction(
+  "[Books API] Books Loaded Success",
+  props<{ books: BookModel[] }>()
+);
+
+export const updateBook = createAction(
+  "[Books API] Book Updated",
+  props<{ book: BookModel }>()
+);
+
+export const createBook = createAction(
+  "[Books API] Book Created",
+  props<{ book: BookModel }>()
+);
+
+export const deleteBook = createAction(
+  "[Books API] Book Deleted",
+  props<{ bookId: string }>()
+);
