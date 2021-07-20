7dd59d1b43c647c0b2f79a41862cf1f1fbef45a8
Daniel Madden <danielfpjobs@gmail.com>
2021-07-20 11:06:30 -0600

    function name changes

--- src/app/books/actions/books-api.actions.ts
+++ src/app/books/actions/books-api.actions.ts
@@ -1,22 +1,22 @@
 import { createAction, props } from "@ngrx/store";
-import { BookModel } from "src/app/shared/models";
+import { BookModel, BookRequiredProps } from "src/app/shared/models";
 
 export const booksLoaded = createAction(
   "[Books API] Books Loaded Success",
   props<{ books: BookModel[] }>()
 );
 
-export const updateBook = createAction(
+export const bookUpdated = createAction(
   "[Books API] Book Updated",
   props<{ book: BookModel }>()
 );
 
-export const createBook = createAction(
+export const bookCreated = createAction(
   "[Books API] Book Created",
   props<{ book: BookModel }>()
 );
 
-export const deleteBook = createAction(
+export const bookDeleted = createAction(
   "[Books API] Book Deleted",
   props<{ bookId: string }>()
 );
