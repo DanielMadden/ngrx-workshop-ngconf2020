e7d8d7ec322f32c13e4350c9eddc6d68bd2dee34
Daniel Madden <danielfpjobs@gmail.com>
2021-07-23 11:13:12 -0600

    Auth Challenge: Step Two - State

--- src/app/shared/state/auth.reducer.ts
+++ src/app/shared/state/auth.reducer.ts
@@ -1,3 +1,38 @@
 import { UserModel } from "../models";
-import { createReducer, on } from "@ngrx/store";
+import { Action, createReducer, on } from "@ngrx/store";
 import { AuthApiActions, AuthUserActions } from "src/app/auth/actions";
+import { LoginEvent } from "src/app/auth/components/login-form";
+
+export interface State {
+  gettingStatus: boolean;
+  user: LoginEvent | null;
+  error: string;
+}
+
+export const initialState: State = {
+  gettingStatus: false,
+  user: null,
+  error: "",
+};
+
+export const authReducer = createReducer(
+  initialState,
+  on(AuthUserActions.logout, (state, action) => {
+    return {
+      gettingStatus: false,
+      user: null,
+      error: "",
+    };
+  }),
+  on(AuthUserActions.login, (state, action) => {
+    return {
+      gettingStatus: true,
+      user: action.account,
+      error: "",
+    };
+  })
+);
+
+export function reducer(state: undefined | State, action: Action) {
+  return authReducer(state, action);
+}
