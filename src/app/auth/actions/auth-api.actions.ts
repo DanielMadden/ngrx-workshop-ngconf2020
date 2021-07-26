8415eb6248b01cbc10e1ca7cb25900d5903d2b1e
Daniel Madden <danielfpjobs@gmail.com>
2021-07-26 11:34:47 -0600

    Auth Challenge: Step Five - Auth API actions

--- src/app/auth/actions/auth-api.actions.ts
+++ src/app/auth/actions/auth-api.actions.ts
@@ -1,2 +1,17 @@
-import { createAction } from "@ngrx/store";
+import { createAction, props } from "@ngrx/store";
 import { UserModel } from "src/app/shared/models";
+
+export const getStatusSuccess = createAction(
+  "[Auth API] Status Success",
+  props<{ user: UserModel | null }>()
+);
+
+export const loginSuccess = createAction(
+  "[Auth API] Login Success",
+  props<{ user: UserModel }>()
+);
+
+export const loginFailure = createAction(
+  "[Auth API] Login Failure",
+  props<{ error: string }>()
+);
